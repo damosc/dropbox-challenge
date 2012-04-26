244217e8eb94a9559931b1353c2ecca8448422fc
Damian Oscik <damosc@wp.pl>
2012-04-26 16:54:07 +0200

    Changes in readme, Some changes in messages

--- src/mypackage/solver.py
+++ src/mypackage/solver.py
@@ -6,9 +6,10 @@ Created on 06-04-2012
 from table import Table
 from rectangle import Rectangles
 import time
-from sys import stdin, stderr
+from sys import stdin, stderr, stdout
 
 if __name__ == '__main__':
+    stdout.write("Please enter number of rectangles\n")
     COUNT = stdin.readline()
     try:
         COUNT = int(COUNT)
@@ -16,7 +17,9 @@ if __name__ == '__main__':
         exit("Wrong format!\n")
     if COUNT < 1 or COUNT > 100:
         exit("Wrong number!\n\Number should be between 1 and 100.\n")
-        
+    
+    stdout.write("Enter width and height of all rectangles separated by space\n")
+    stdout.write("After every rectangle press [Enter]\n")
     RECTANGLES = Rectangles()
     for i in range(COUNT):
         line = stdin.readline()
@@ -32,7 +35,7 @@ if __name__ == '__main__':
     
     START_TIME = time.time()
     TABLE = Table(rectangles=RECTANGLES)
-    stderr.write("Solving...\n")
+    stdout.write("Solving...\n")
     TABLE.find_solution()
     END_TIME = time.time()
     stderr.write("Execution time: " + str(END_TIME - START_TIME) + "\n")
